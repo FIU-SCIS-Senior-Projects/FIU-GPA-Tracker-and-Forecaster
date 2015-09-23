f4ee5e5b6b07d36d0f66ff099029b884874a201b
jmach019 <jmach019@fiu.edu>
2015-09-23 12:28:44 -0400

    Story 699: First push into this branch also includes earlier stories671, 699, 700, 701, 705

---
+++ Code/WebSite/fiugpatf/nav.php
@@ -0,0 +1,35 @@
+<?php
+include_once 'psl-config.php';
+include_once 'functions.php';
+
+sec_session_start();
+
+if(isset($_SESSION['username']))
+{
+    $username = $_SESSION['username'];
+    $conn = new mysqli(HOST, USER, PASSWORD, DATABASE);
+    if($stmt = $conn->prepare("SELECT *
+                               From admin
+                               Where username = ?")) {
+        $stmt->bind_param('s', $username);
+        $stmt->execute();
+        $stmt->store_result();
+        $stmt->bind_result($username);
+        $stmt->fetch();
+
+        if ($stmt->num_rows() > 0) {
+            echo "<li><a href=#>Admin</a></li>";
+        }
+    }
+
+    echo "<li><a href=#>GPA Dashboard</a></li>";
+    echo "<li><a href=#>Grade Dashboard</a></li>";
+    echo "<li><a href=#>Logout</a></li>";
+}
+else
+{
+    echo "<li><a href=\"register.html\">Register</a></li>";
+    echo "<li><a href=\"login.html\">Log in</a></li>";
+    echo "<li><a href=#>About</a></li>";
+}
+?>
---
+++ Code/WebSite/fiugpatf/psl-config.php
@@ -0,0 +1,16 @@
+
+
+<?php
+/**
+ * These are the database login details
+ */  
+define("HOST", "localhost");     // The host you wa
+define("USER", "sec_user");    // The database username. 
+define("PASSWORD", "Uzg82t=u%#bNgPJw");    // The database password. 
+define("DATABASE", "GPA_Tracker");    // The database name.
+ 
+define("CAN_REGISTER", "any");
+define("DEFAULT_ROLE", "member");
+ 
+define("SECURE", FALSE);    // FOR DEVELOPMENT ONLY!!!!
+?>
