55c4df11c74d9252574c6c01cfbb0d5f7c5f9f16
jmach019 <jmach019@fiu.edu>
2015-11-07 13:35:32 -0500

    Story 730: Student data import/export/delete functionality

--- Code/WebSite/fiugpatf/common_files/settings.php
+++ Code/WebSite/fiugpatf/common_files/settings.php
@@ -33,6 +33,11 @@ if($action == "prepareTable") {
 		array_push($output, array("Change Major", ""));
 		array_push($output, array("Change Themes", ""));
 		array_push($output, array("Export Data", '<button type="button" id="ExportButton">Export Data</button>'));
+<<<<<<< HEAD
+		array_push($output, array("Import Data", '<input type="file" id="ImportFile">'));
+		//array_push($output, array("Import Whatif", '<input type="file" id="Whatif">'));
+=======
+>>>>>>> Story 733: Current Semester GUI improvements Set 1
 
 		echo json_encode($output);
 	}
@@ -45,4 +50,136 @@ if($action == "exportData") {
 		echo $dump;
 	}
 }
+<<<<<<< HEAD
+
+if($action == "importData") {
+	if(isset($_SESSION['username'])){
+		$xml = simplexml_load_string($_POST['file']);
+		if($xml === false)
+		{
+			echo "Failed loading XML: ";
+    		foreach(libxml_get_errors() as $error) {
+        		echo "<br>", $error->message;
+			}
+		}
+		else
+		{
+			if($xml->database[0]['name'] == 'GPA_Tracker'){
+				if(validate_and_insert_data($xml)) {
+					echo "true";
+				}
+				else
+				{
+					echo "Error!";
+				}
+			}
+			else {
+				echo "Data can only be inserted into GPA_Tracker.";
+			}
+		}
+	}
+}
+
+/*if($action == "importWhatif") {
+	if(isset($_SESSION['username'])){
+		$filename = $_SESSION['username'] . 'Whatif';
+		$wifile = fopen($filename, $_POST['file']);
+		fwrite($wifile, $wifile);
+		fclose($filename);
+		$xml = simplexml_load_string(shell_exec('python3 WhatIfParser.py ' . $_SESSION['username'] . ' ' . $filename));
+		if($xml === false)
+		{
+			echo "Failed loading XML: ";
+    		foreach(libxml_get_errors() as $error) {
+        		echo "<br>", $error->message;
+			}
+		}
+		else
+		{
+			if($xml->database[0]['name'] == 'GPA_Tracker'){
+				if(insert_student_data($xml)) {
+					echo "true";
+				}
+				else
+				{
+					echo "Error!";
+				}
+			}
+			else {
+				echo "Data can only be inserted into GPA_Tracker.";
+			}
+		}
+		shell_exec('rm ' . $filename)
+	}
+}*/
+
+function validate_and_insert_data($xml)
+{
+	$mysqli = new mysqli("localhost","sec_user","Uzg82t=u%#bNgPJw","GPA_Tracker");
+	$user = $_SESSION['username'];
+	foreach($xml->database[0]->children() as $table_data)
+	{
+		foreach($table_data->children() as $rows)
+		{
+			if($table_data['name'] == 'student_data')
+			{
+				if($rows->field[1] != $user)
+				{
+					return false;
+				}
+			}
+			else if($table_data['name'] == 'student_course')
+			{
+				if($rows->field[0] != $user)
+				{
+					return false;
+				}
+				$stmt = $mysqli->prepare("INSERT INTO student_course (username, courseID, grade, weight, relevance, student_course_id, semester, year) VALUES (?, ?, ?, ?, ?, ?, ?, ?)
+                                          ON DUPLICATE KEY UPDATE grade=VALUES(grade), weight=VALUES(weight), relevance=VALUES(relevance)");
+				$stmt->bind_param('ssssssss', $user, $rows->field[1], $rows->field[2], $rows->field[3], $rows->field[4], $rows->field[5], $rows->field[6], $rows->field[7]);
+    			$stmt->execute();
+			}
+			else if($table_data['name'] == 'student_major')
+			{
+				if($rows->field[0] != $user)
+				{
+					return false;
+				}
+				$stmt = $mysqli->prepare("INSERT INTO student_major (username, major_id, declared_month, declared_year) VALUES (?, ?, ?, ?)
+                                          ON DUPLICATE KEY UPDATE declared_month=VALUES(declared_month), declared_year=VALUES(declared_year)");
+				$stmt->bind_param('ssss', $user, $rows->field[1], $rows->field[2], $rows->field[3]);
+    			$stmt->execute();
+			}
+			else if($table_data['name'] == 'assessment_type')
+			{
+				if($rows->field[0] != $user)
+				{
+					return false;
+				}
+				$stmt = $mysqli->prepare("INSERT INTO assessment_type (courseID, assessment, percentage, student_course_id) VALUES (?, ?, ?, ?)
+                                          ON DUPLICATE KEY UPDATE assessment=VALUES(assessment), percentage=VALUES(percentage)");
+				$stmt->bind_param('ssss', $rows->field[1], $rows->field[2], $rows->field[3], $rows->field[4]);
+    			$stmt->execute();
+			}
+		}
+	}
+	return true;
+}
+
+/*insert_student_data($xml) {
+	$mysqli = new mysqli("localhost","sec_user","Uzg82t=u%#bNgPJw","GPA_Tracker");
+	$user = $_SESSION['username'];
+	foreach($xml->database[0]->children() as $table_data)
+	{
+		foreach($table_data->children() as $rows)
+		{
+			$stmt = $mysqli->prepare("INSERT INTO student_course (username, courseID, grade, weight, relevance, student_course_id, semester, year) VALUES (?, ?, ?, ?, ?)
+                                          ON DUPLICATE KEY UPDATE grade=VALUES(grade), weight=VALUES(weight), relevance=VALUES(relevance)");
+				$stmt->bind_param('sssssss', $user, $rows->field[1], $rows->field[2], $rows->field[3], $rows->field[4], $rows->field[5], $rows->field[6]);
+    			$stmt->execute();
+		}
+	}
+}*/
+=======
+>>>>>>> Story 733: Current Semester GUI improvements Set 1
 ?>
