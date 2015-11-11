d9cac906c618966c63553576e561d7cd1cc20ef3
jmach019 <jmach019@fiu.edu>
2015-11-10 20:27:42 -0500

    Story 768: Help Feature for Semester Dashboard

--- Code/WebSite/fiugpatf/overallgpadashboard/nav.php
+++ Code/WebSite/fiugpatf/overallgpadashboard/nav.php
@@ -5,22 +5,7 @@ include_once '../common_files/functions.php';
 sec_session_start();
 
 if (isset($_SESSION['username'])) {
-    $username = $_SESSION['username'];
-    $conn     = new mysqli(HOST, USER, PASSWORD, DATABASE);
-    if ($stmt = $conn->prepare("SELECT *
-                               From admin
-                               Where username = ?")) {
-        $stmt->bind_param('s', $username);
-        $stmt->execute();
-        $stmt->store_result();
-        $stmt->bind_result($username);
-        $stmt->fetch();
-        
-        if ($stmt->num_rows() > 0) {
-            echo "<li><a href=#>Admin</a></li>";
-        }
-    }    
-    echo "<li><a href=\"./OvrlDash.html\">GPA Dashboard</a></li>";
+	echo "<li><a href=\"./OvrlDash.html\">GPA Dashboard</a></li>";
     echo "<li><a href=\"../sem_dashboard/current.html\">Grade Dashboard</a></li>";
     echo "<li><a href=#>Logout</a></li>";
 } else {
