48474ad174ec1e0273292dc8b3b1c4df50a287b9
jmach019 <jmach019@fiu.edu>
2015-12-05 18:33:51 -0500

    Cleaning up navigation

--- Code/WebSite/fiugpatf/nav.php
+++ Code/WebSite/fiugpatf/nav.php
@@ -6,16 +6,16 @@ sec_session_start();
 
 if(isset($_SESSION['username']))
 {
-    echo "<li><a href=\"../overallgpadashboard/OvrlDash.html\">GPA Dashboard</a></li>";
-    echo "<li><a href=\"../sem_dashboard/current.html\">Semester Dashboard</a></li>";
-	echo "<li><a href=\"../common_files/settings.html\">Settings</a></li>";
+    echo "<li><a href=\"/overallgpadashboard/OvrlDash.html\">GPA Dashboard</a></li>";
+    echo "<li><a href=\"/sem_dashboard/current.html\">Semester Dashboard</a></li>";
+	echo "<li><a href=\"/common_files/settings.html\">Settings</a></li>";
 
-    echo "<li><a href=\"login.html\">Logout</a></li>";
+    echo "<li><a href=\"/common_files/logout.php\">Logout</a></li>";
 }
 else
 {
     echo "<li><a href=\"common_files/register.html\">Register</a></li>";
     echo "<li><a href=\"login.html\">Log in</a></li>";
-    echo "<li><a href=#>About</a></li>";
+    echo "<li><a href=\"/overallgpadashboard/about.html\">About</a></li>";
 }
 ?>
--- Code/WebSite/fiugpatf/sem_dashboard/nav.php
+++ Code/WebSite/fiugpatf/sem_dashboard/nav.php
@@ -9,7 +9,24 @@ if(isset($_SESSION['username']))
     echo "<li><a href=\"../overallgpadashboard/OvrlDash.html\">GPA Dashboard</a></li>";
     echo "<li><a href=\"./current.html\">Semester Dashboard</a></li>";
 	echo "<li><a href=\"../common_files/settings.html\">Settings</a></li>";
-    echo "<li><a href=#>Logout</a></li>";
+    echo "<li><a href=\"../common_files/logout.php\">Logout</a></li>";
+    
+    echo '<li>';
+    echo '  <div class="dropdown">';
+    echo '    <a class="btn btn-default dropdown-toggle" data-toggle="dropdown">Help <span class="caret"></span></a>';
+    echo '      <ul class="dropdown-menu" style="margin-top: 1px;">';
+    echo '        <li>';
+    echo '          <a href="../overallgpadashboard/OvrlDash_help_contents.html">GPA Dashboard Help</a>';
+    echo '        </li>';
+    echo '        <li>';
+    echo '          <a href="../sem_dashboard/SemHelpContents.html">Semester Dashboard Help</a>';
+    echo '        </li>';
+    echo '        <li>';
+    echo '          <a href="../overallgpadashboard/about.html">About</a>';
+    echo '        </li>';
+    echo '      </ul>';
+    echo '  </div>';
+    echo '</li>';
 }
 else
 {
